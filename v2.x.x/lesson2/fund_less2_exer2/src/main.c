7660e73f2312b190bd2b4f68697b4de83f74ad9f
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-11-08 14:00:05 +0000

    lesson 2 exercise 2 complete

--- v2.x.x/lesson2/fund_less2_exer2/src/main.c
+++ v2.x.x/lesson2/fund_less2_exer2/src/main.c
@@ -10,7 +10,9 @@
 #include <zephyr/drivers/gpio.h>
 
 /* STEP 9 - Increase the sleep time from 100ms to 10 minutes  */
-#define SLEEP_TIME_MS   100
+#define SLEEP_TIME_MINS	(10)
+#define SLEEP_TIME_SECS	(SLEEP_TIME_MINS * 60)
+#define SLEEP_TIME_MS   (SLEEP_TIME_SECS * 1000)
 
 /* SW0_NODE is the devicetree node identifier for the "sw0" alias */
 #define SW0_NODE	DT_ALIAS(sw0) 
@@ -19,18 +21,28 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 /* LED0_NODE is the devicetree node identifier for the "led0" alias. */
 #define LED0_NODE	DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define LED1_NODE	DT_ALIAS(led1)
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 
 
 /* STEP 4 - Define the callback function */
+void button_pressed(const struct device * dev, struct gpio_callback *cb, uint32_t pins)
+{
+	gpio_pin_toggle_dt(&led);
+}
 
 /* STEP 5 - Define a variable of type static struct gpio_callback */
+static struct gpio_callback button_cb_data;
 
 void main(void)
 {
 	int ret;
 
 	if (!device_is_ready(led.port)) {
+		return;
+	}
+	if (!device_is_ready(led1.port)) {
 		return;
 	}
 
@@ -42,22 +54,30 @@ void main(void)
 	if (ret < 0) {
 		return;
 	}
+	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret < 0) {
 		return;
 	}
 	/* STEP 3 - Configure the interrupt on the button's pin */
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
 
 	/* STEP 6 - Initialize the static struct gpio_callback variable   */
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
 
 	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+	gpio_add_callback(button.port, &button_cb_data);
 
-	while (1) {
-		/* STEP 8 - Remove the polling code */
-        bool val = gpio_pin_get_dt(&button);
-        gpio_pin_set_dt(&led,val);		
-     
+	while (true) {
+        gpio_pin_toggle_dt(&led1);
+
         k_msleep(SLEEP_TIME_MS); 
 	}
 }

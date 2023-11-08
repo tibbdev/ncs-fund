4b473d5cb04f302ca0d9c9e6418b74f1f7546516
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-11-08 14:52:48 +0000

    lesson 3 exercise 2 completed

---
+++ v2.x.x/lesson3/fund_less3_exer1/src/func.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// returns the arithmetic sum of 'a' and 'b'
+int sum(int a, int b);
--- v2.x.x/lesson3/fund_less3_exer1/src/main.c
+++ v2.x.x/lesson3/fund_less3_exer1/src/main.c
@@ -1,11 +1,28 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+#ifdef CONFIG_MYFUNCTION
+    #include "func.h"
+#endif
+
 void main(void)
 {
+    #ifdef CONFIG_MYFUNCTION
+        int a = 1;
+        int b = 2;
+        int temp;
+    #endif
+
     while(1)
     {
-        printk("Hello World!\r\n");
+        #ifdef CONFIG_MYFUNCTION
+            printk("\r\nThe sum of %d and %d is %d", a, b, sum(a, b));
+            temp = a;
+            a = sum(a, b) % 512;
+            b++;
+        #else
+            printk("\r\nMYFUNCTION not enabled");
+        #endif
         k_msleep(1000);
     }
 }

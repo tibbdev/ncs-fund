239372af3006cf3e09773a73965f7708fc2c8aee
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-11-13 12:59:09 +0000

    Lesson 8 Exercise 2 completed -> Mutexs unleashed

--- v2.x.x/lesson8/fund_less8_exer2/src/main.c
+++ v2.x.x/lesson8/fund_less8_exer2/src/main.c
@@ -13,27 +13,64 @@
 #define THREAD1_STACKSIZE       512
 
 /* STEP 3 - Set the priority of the two threads to have equal priority*/
+#define THREAD0_PRIORITY        4 
+#define THREAD1_PRIORITY        4
 
 /* STEP 5 - Define the two counters with a constant combined total */
+#define COMBINED_TOTAL   40
+int32_t increment_count = 0; 
+int32_t decrement_count = COMBINED_TOTAL; 
 
 /* STEP 11 - Define mutex to protect access to shared code section */
+K_MUTEX_DEFINE(test_mutex);
 
 // Shared code run by both threads
 void shared_code_section(void)
 {
 	/* STEP 12.1 - Lock the mutex */
+	k_mutex_lock(&test_mutex, K_FOREVER);
 
 	/* STEP 6 - Increment count and decrement count changed */
 	/* according to logic defined in exercise text */
+	increment_count++;
+	increment_count = increment_count % COMBINED_TOTAL;
+	decrement_count--;
+	if (decrement_count == 0)
+	{
+		decrement_count = COMBINED_TOTAL;
+	}
 
 	/* STEP 12.2 - Unlock the mutex */
+	k_mutex_unlock(&test_mutex);
 
 	/* STEP 7 - Print counter values if they do not add up to COMBINED_TOTAL */
+	if(COMBINED_TOTAL != (increment_count + decrement_count))
+	{
+		printk("Race Condition Happened!\r\n");
+		printk("\tincrement_count : %d\r\n", increment_count);
+		printk("\tdecrement_count : %d\r\n", decrement_count);
+		printk("\tcombined_total = %d\r\n", increment_count + decrement_count);
 
+		k_msleep(400 + sys_rand32_get() % 10);
+	}
 }
 
 /* STEP 4 - Functions for thread0 and thread1 with a shared code section */
+void thread0(void)
+{
+	printk("Thread 0 started\n");
+	while (1) {
+		shared_code_section(); 
+	}
+}
 
+void thread1(void)
+{
+	printk("Thread 1 started\n");
+	while (1) {
+		shared_code_section(); 
+	}
+}
 
 // Define and initialize threads
 K_THREAD_DEFINE(thread0_id, THREAD0_STACKSIZE, thread0, NULL, NULL, NULL,

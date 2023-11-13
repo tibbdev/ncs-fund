ab802943d8d3816647efc5bb65bd92597e357baf
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-11-13 11:50:17 +0000

    Lesson 7 exercise 2 completed...

--- v2.x.x/lesson7/fund_less7_exer2/src/main.c
+++ v2.x.x/lesson7/fund_less7_exer2/src/main.c
@@ -10,27 +10,47 @@
 #define STACKSIZE 1024
 
 /* STEP 5 - Change the priority of thread0 to 6 */
-#define THREAD0_PRIORITY 7
+#define THREAD0_PRIORITY 6
 #define THREAD1_PRIORITY 7
 
 void thread0(void)
 {
-	while (1) {
-          printk("Hello, I am thread0\n");
-		  k_busy_wait(1000000);
+	while (1) 
+   {
+      printk("Hello, I am thread0\n");
+      k_busy_wait(1000000);
 	}
 }
 
 void thread1(void)
 {
-	while (1) {
-          printk("Hello, I am thread1\n");
-		  k_busy_wait(1000000);
+	while (1) 
+   {
+      printk("Hello, I am thread1\n");
+      k_busy_wait(1000000);
 	}
 }
 
-K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL,
-		THREAD0_PRIORITY, 0, 0);
-K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL,
-		THREAD1_PRIORITY, 0, 0);
+K_THREAD_DEFINE(
+      thread0_id, 
+      STACKSIZE,
+      thread0,
+      NULL, 
+      NULL, 
+      NULL,
+      THREAD0_PRIORITY,
+      0,
+      0
+   );
+K_THREAD_DEFINE(
+      thread1_id, 
+      STACKSIZE, 
+      thread1, 
+      NULL, 
+      NULL, 
+      NULL,
+		THREAD1_PRIORITY, 
+      0, 
+      0
+   );
 

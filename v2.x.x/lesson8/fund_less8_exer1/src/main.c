12bbafff40c9135bde96f117809d2ed0c02921bc
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-11-13 12:44:59 +0000

    Completed Exercise 1 of Lesson 8 -> Semaphores in RTOSs

--- v2.x.x/lesson8/fund_less8_exer1/src/main.c
+++ v2.x.x/lesson8/fund_less8_exer1/src/main.c
@@ -9,43 +9,61 @@
 #include <zephyr/random/rand32.h>
 #include <string.h>
 
-#define PRODUCER_STACKSIZE       512
-#define CONSUMER_STACKSIZE       512
+#define PRODUCER_STACKSIZE      512
+#define CONSUMER_STACKSIZE		512
 
 /* STEP 2 - Set the priority of the producer and consumer thread */
+#define PRODUCER_PRIORITY		5
+#define CONSUMER_PRIORITY		4
 
 /* STEP 9 - Define semaphore to monitor instances of available resource */
+K_SEM_DEFINE(instance_monitor_sem, 10, 10);
 
 /* STEP 3 - Initialize the available instances of this resource */
+volatile uint32_t available_instance_count = 10;
 
 // Function for getting access of resource
 void get_access(void)
 {
 	/* STEP 10.1 - Get semaphore before access to the resource */
+	k_sem_take(&instance_monitor_sem, K_FOREVER);
 
 	/* STEP 6.1 - Decrement available resource */
-
+	available_instance_count--;
+	printk("Resource taken and available_instance_count = %d\r\n", available_instance_count);
 }
 
 // Function for releasing access of resource
 void release_access(void)
 {
 	/* STEP 6.2 - Increment available resource */
+	available_instance_count++;
+	printk("Resource released and available_instance_count = %d\r\n", available_instance_count);
 
 	/* STEP 10.2 - Give semaphore after finishing access to resource */
-
+	k_sem_give(&instance_monitor_sem);
 }
 
 /* STEP 4 - Producer thread relinquishing access to instance */
 void producer(void)
 {
-
+	printk("Producer thread started\r\n");
+	while (true)
+	{
+		release_access();
+		k_msleep(500 + (sys_rand32_get() % 10));
+	}
 }
 
 /* STEP 5 - Consumer thread obtaining access to instance */
 void consumer(void)
 {
-
+	printk("Consumer thread started\r\n");
+	while (true)
+	{
+		get_access();
+		k_msleep(sys_rand32_get() % 10);
+	}
 }
 
 // Define and initialize threads

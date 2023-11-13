e42ff61f7d7196a230b216ae81a9ad90595c4493
M. Tibbitts <marcus.tibbitts@dyson.com>
2023-11-13 11:40:31 +0000

    Completed Exercise 1 from Lesson 7

--- v2.x.x/lesson7/fund_less7_exer1/src/main.c
+++ v2.x.x/lesson7/fund_less7_exer1/src/main.c
@@ -8,25 +8,38 @@
 #include <zephyr/sys/printk.h>
 
 /* STEP 2 - Define stack size and scheduling priority used by each thread */
+#define STACKSIZE			(1024)
+#define THREAD0_PRIORITY	(7)
+#define THREAD1_PRIORITY	(7)
 
 void thread0(void)
 {
+   printk("\r\nThread0 Begins\r\n");
 	while (1) {
-		  /* STEP 3 - Call printk() to display a simple string "Hello, I am thread0" */
-		  /* STEP 6 - Make the thread yield */
-		  /* STEP 10 - Put the thread to sleep */
-		  /* Remember to comment out the line from STEP 6 */
+      /* STEP 3 - Call printk() to display a simple string "Hello, I am thread0" */
+		printk("Hello from Thread0!\r\n");
+      /* STEP 6 - Make the thread yield */
+      // k_yield();
+      /* STEP 10 - Put the thread to sleep */
+      k_msleep(5);
+      /* Remember to comment out the line from STEP 6 */
 	}
 }
 
 void thread1(void)
 {
+   printk("Thread1 Begins\r\n");
 	while (1) {
-		  /* STEP 3 - Call printk() to display a simple string "Hello, I am thread1" */
-		  /* STEP 8 - Make the thread yield */
-		  /* STEP 10 - Put the thread to sleep */
-		  /* Remember to comment out the line from STEP 8 */
+      /* STEP 3 - Call printk() to display a simple string "Hello, I am thread1" */
+      printk("Hello from Thread1!\r\n");
+      /* STEP 8 - Make the thread yield */
+      // k_yield();
+      /* STEP 10 - Put the thread to sleep */
+      k_msleep(5);
+      /* Remember to comment out the line from STEP 8 */
 	}
 }
 
 /* STEP 4 - Define and initialize the two threads */
+K_THREAD_DEFINE(thread0_id, STACKSIZE, thread0, NULL, NULL, NULL, THREAD0_PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL, THREAD1_PRIORITY, 0, 0);
